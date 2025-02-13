4b0bb25c2d7c3f41e28e30b6f6e2675e4ad9ebb5
Elizaveta German <elizavetagerman@M1.local>
2025-02-13 16:03:19 +0300

    Инициализация проекта и создание .gitignore

---
+++ src/widget.py
@@ -0,0 +1,16 @@
+# src/widget.py
+from masks import mask_card, mask_account  # Предполагается, что эти функции уже существуют
+
+def mask_account_card(account_info: str) -> str:
+    """Маскирует номер карты или счета."""
+    account_type, account_number = account_info.split(' ', 1)
+    if 'Счет' in account_type:
+        return mask_account(account_number)
+    else:
+        return mask_card(account_number)
+
+def get_date(date_str: str) -> str:
+    """Преобразует строку даты в формат 'ДД.ММ.ГГГГ'."""
+    from datetime import datetime
+    date_obj = datetime.fromisoformat(date_str)
+    return date_obj.strftime("%d.%m.%Y")
