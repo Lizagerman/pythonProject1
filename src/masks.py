981186cc40b30107f0ee4334c90be741c35094e8
Elizaveta German <elizavetagerman@m1.local>
2025-02-17 11:38:22 +0300

    init1

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,10 @@
 def get_mask_card_number(card_number: str) -> str:
-    """Замаскировать номер банковской карты, оставляя последние 4 цифры видимыми."""
+    """Замаскировать номер банковской карты,
+    оставляя последние 4 цифры видимыми."""
     return "*" * (len(card_number) - 4) + card_number[-4:]
 
-def get_mask_account(account_number: str) -> str:
-    """Замаскировать номер банковского счета, оставляя последние 4 цифры видимыми."""
-    return "*" * (len(account_number) - 4) + account_number[-4:]
 
+def get_mask_account(account_number: str) -> str:
+    """Замаскировать номер банковского счета,
+    оставляя последние 4 цифры видимыми."""
+    return "*" * (len(account_number) - 4) + account_number[-4:]
--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,15 @@
+
 # src/widget.py
-from masks import mask_card, mask_account  # Предполагается, что эти функции уже существуют
+
 
 def mask_account_card(account_info: str) -> str:
     """Маскирует номер карты или счета."""
     account_type, account_number = account_info.split(' ', 1)
-    if 'Счет' in account_type:
-        return mask_account(account_number)
+    if 'Счет' not in account_type:
+        return mask_account_card(account_number)
     else:
-        return mask_card(account_number)
+        return mask_account_card(account_number)
+
 
 def get_date(date_str: str) -> str:
     """Преобразует строку даты в формат 'ДД.ММ.ГГГГ'."""
